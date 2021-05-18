543c16c86c20bed89456075df423f4b2dfd88f5d
bkjml <mattbirhane@gmail.com>
2021-05-18 22:05:43 +0300

    4 files modified, 3 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Product } from '../products';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent {
+  @Input() product!: Product;
+  @Output() notify = new EventEmitter();
+}
